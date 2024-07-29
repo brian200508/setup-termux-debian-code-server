4e43b82af2f0b78458eddafbea29860323ff12d3
brian200508 <36002167+brian200508@users.noreply.github.com>
2024-07-29 16:57:07 +0200

    fix install

--- install-debian.sh
+++ install-debian.sh
@@ -9,6 +9,7 @@ SHORTCUTS_DIR=.shortcuts
 #termux-setup-storage
 apt update -y
 apt install -y curl wget
+apt update -y && apt upgrade -y
 echo "Downloading code-server..."
 cd ~
 #FIXME: wget -O ~/code-server-install.deb 'https://github.com/coder/code-server/releases/download/v${VERSION}code-server_${VERSION}_arm64.deb'
@@ -28,6 +29,7 @@ cp $REPO_DIR_TERMUX/$REPO_NAME/shortcuts/icons/debian-code-server.sh.png $SHORTC
 echo "Setting up Proot-Distro Debian..."
 apt install -y git build-essential
 apt install -y proot-distro
+apt update -y && apt upgrade -y
 proot-distro install debian
 proot-distro login debian -- apt update -y
 proot-distro login debian -- apt install -y git build-essential python-is-python3 python3 python3-pip python3-venv nodejs npm
